a59b2af0c56c78697b97c6a7d9e94ff78d9120fb
yuvraj <46413950+y4yuvraj@users.noreply.github.com>
2024-02-11 18:46:06 +0530

    lifecycle method using interface

---
+++ springLifecycleMethods/src/main/java/com/yuv/springLifecycleMethodUsingAnnotation/Employee.java
@@ -0,0 +1,48 @@
+package com.yuv.springLifecycleMethodUsingAnnotation;
+
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+public class Employee implements InitializingBean, DisposableBean {
+
+	private int id;
+	private String name;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + ", name=" + name + "]";
+	}
+	
+//	public void hi()
+//	{
+//		System.out.println("Initializing method");
+//	}
+//	
+//	public void bye()
+//	{
+//		System.out.println("Destruction method");
+//	}
+	@Override
+	public void destroy() throws Exception {
+		System.out.println("Initializing method");
+		
+	}
+	@Override
+	public void afterPropertiesSet() throws Exception {
+		System.out.println("Destruction method");
+		
+	}
+	
+}
---
+++ springLifecycleMethods/src/main/java/com/yuv/springLifecycleMethodUsingInterface/Main.java
@@ -0,0 +1,15 @@
+package com.yuv.springLifecycleMethodUsingInterface;
+
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+
+	public static void main(String[] args) {
+		AbstractApplicationContext context=new ClassPathXmlApplicationContext("com/yuv/springLifecycleMethodUsingInterface/config.xml");
+		Employee emp = (Employee) context.getBean("emp");
+		System.out.println(emp);
+		context.registerShutdownHook();
+	}
+
+}
