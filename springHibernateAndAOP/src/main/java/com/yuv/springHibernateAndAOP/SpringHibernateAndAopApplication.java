0d02fef1171b81cff1e272962f150034b80ba69f
yuvraj <46413950+y4yuvraj@users.noreply.github.com>
2024-04-18 19:35:14 +0530

    springhibernate and AOP
    aspect oriented programming

---
+++ springHibernateAndAOP/src/main/java/com/yuv/springHibernateAndAOP/SpringHibernateAndAopApplication.java
@@ -0,0 +1,13 @@
+package com.yuv.springHibernateAndAOP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHibernateAndAopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringHibernateAndAopApplication.class, args);
+	}
+
+}
---
+++ springHibernateAndAOP/src/test/java/com/yuv/springHibernateAndAOP/SpringHibernateAndAopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yuv.springHibernateAndAOP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringHibernateAndAopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
